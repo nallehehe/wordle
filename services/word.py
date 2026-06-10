6d7d0c8b62b48f43335ab1b04d7587a88c73c845
alex <nalleballeuwu@gmail.com>
2026-06-10 02:54:45 +0200

    checks if word exists in the db

--- services/word.py
+++ services/word.py
@@ -3,8 +3,11 @@ from fastapi import HTTPException
 
 
 async def insert_word(name: str):
+    existing_word = await Word.find_one(Word.name == name)
+    if existing_word:
+        raise HTTPException(status_code=409, detail="Word already exists")
     if len(name) != 5:
-        raise HTTPException(status_code=404, detail="Word must be 5 letters")
+        raise HTTPException(status_code=400, detail="Word must be 5 letters")
     word = Word(name=name)
     await word.insert()
     return word
