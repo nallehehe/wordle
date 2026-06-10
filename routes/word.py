b05da63111ae86a809442fe836f106ee44cfef2a
alex <nalleballeuwu@gmail.com>
2026-06-10 13:11:43 +0200

    fetch specific words

--- routes/word.py
+++ routes/word.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import Word
 from services import insert_word
+from beanie import PydanticObjectId
+
 
 router = APIRouter()
 
@@ -9,7 +11,16 @@ router = APIRouter()
 async def create_word(body: Word):
     return await insert_word(body.name)
 
+
 @router.get("/words")
 async def get_words():
     words = await Word.find_all().to_list()
     return words
+
+
+@router.get("/word/{word_id}")
+async def get_word(word_id: PydanticObjectId):
+    word = await Word.get(word_id)
+    if not word:
+        raise HTTPException(status_code=404, detail="Word not found")
+    return word
