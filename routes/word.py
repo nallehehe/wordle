12198b86f983b1509811136bd83aa516f161460e
alex <nalleballeuwu@gmail.com>
2026-06-10 12:58:46 +0200

    fetch all route

--- routes/word.py
+++ routes/word.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.post("/word")
 async def create_word(body: Word):
     return await insert_word(body.name)
+
+@router.get("/words")
+async def get_words():
+    words = await Word.find_all().to_list()
+    return words
