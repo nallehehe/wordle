103e779a4c2a749eada850a9081ef7623eb69106
alex <nalleballeuwu@gmail.com>
2026-06-18 01:16:44 +0200

    get single game

--- routes/game.py
+++ routes/game.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services import new_game
+from beanie import PydanticObjectId
+from models import Game
+
 
 router = APIRouter()
 
@@ -7,3 +10,11 @@ router = APIRouter()
 @router.post("/game")
 async def create_word():
     return await new_game()
+
+
+@router.get("/game/{game_id}")
+async def get_game(game_id: PydanticObjectId):
+    game = await Game.get(game_id)
+    if not game:
+        raise HTTPException(status_code=404, detail="Game not found")
+    return game
